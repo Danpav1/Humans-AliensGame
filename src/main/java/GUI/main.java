79a181d35ddbd8f4ab431f3982612874a6cf9dcc
Daniel Pavenko <pavenkodanielofficial@hotmail.com>
2023-11-10 19:39:41 -0500

    created GUI package that holds main method and board GUI, created main method, created board GUIclass & added functionality to said class, created assets package that holds the icons

---
+++ src/main/java/GUI/main.java
@@ -0,0 +1,17 @@
+package GUI;
+
+/**
+ * main class that only houses the main method.
+ */
+
+public class main {
+  
+  /**
+   * main method that "starts" the GUI
+   * @param args
+   */
+  public static void main(String[] args) {
+    gameUIBoard guiBoard = new gameUIBoard();
+    guiBoard.startUI();
+  }
+}
