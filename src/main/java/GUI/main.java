cb2068617922ce2b7a3f056149d5ea197cc51b24
Daniel Pavenko <pavenkodanielofficial@hotmail.com>
2023-11-13 23:13:47 -0500

    making environments constructor goes against singleton pattern, used the getEnvironment method instead

--- src/main/java/GUI/main.java
+++ src/main/java/GUI/main.java
@@ -22,7 +22,7 @@ public class main {
    * @throws RecoveryRateException
    */
   public static void main(String[] args) throws RecoveryRateException {
-    Environment world = new Environment(6, 6); //creates our "world" // board that the GUI references
+    Environment world = Environment.getEnvironment(6, 6); //creates our "world" // board that the GUI references
     LifeForm alien = new Alien("Ligma", 100); 
     LifeForm human = new Human("Sugma", 100, 10);
     Weapon plasmaCannon = new PlasmaCannon();
