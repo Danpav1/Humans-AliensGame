e807e8d2573190526cd540aa6b9bcc6e9ee69aa3
Garrett Goshorn <gg4763@engr.ship.edu>
2023-11-17 11:04:25 -0500

    Tested alien attack when human has armor

--- src/test/java/lifeform/TestAlien.java
+++ src/test/java/lifeform/TestAlien.java
@@ -3,10 +3,12 @@ package lifeform;
 import static org.junit.Assert.*;
 
 import exceptions.RecoveryRateException;
+import exceptions.WeaponException;
 import gameplay.SimpleTimer;
 import org.junit.Test;
 
 import recovery.*;
+import weapon.Pistol;
 
 /**
  * Tests the functionality provided by the Alien class
@@ -23,6 +25,30 @@ public class TestAlien {
   public void testMaxSpeed() throws RecoveryRateException {
     Alien alien = new Alien("Alien", 30, new RecoveryNone());
     assertEquals(2, alien.getMaxSpeed());
+  }
+
+  @Test
+  public void testAlienAttack() throws RecoveryRateException, WeaponException {
+    Alien alien = new Alien("Sugma",30, new RecoveryNone());
+    Human human = new Human("Ligma",50,0);
+    alien.attack(human,0);
+    assertEquals(40, human.getCurrentLifePoints());
+
+    Pistol pistol = new Pistol();
+    alien.pickUpWeapon(pistol);
+    alien.attack(human,10);
+    assertEquals(30, human.getCurrentLifePoints());
+  }
+
+  @Test
+  public void testAlienAttackWithArmor() throws RecoveryRateException, WeaponException {
+    Alien alien = new Alien("Sugma",30, new RecoveryNone());
+    Human human = new Human("Ligma",50,10);
+    Pistol pistol = new Pistol();
+
+    alien.pickUpWeapon(pistol);
+    alien.attack(human, 0);
+    assertEquals(48, human.getCurrentLifePoints());
   }
 
   /*
